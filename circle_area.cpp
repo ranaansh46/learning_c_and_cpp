4571dc56bfff07ca884c00cf7d39e147f544ff1e
Ansh Rana <60007155+ranaansh46@users.noreply.github.com>
2021-09-20 21:07:24 +0530

    Add files via upload

---
+++ circle_area.cpp
@@ -0,0 +1,37 @@
+#include <iostream>
+const float pi=3.1452;
+using namespace std;
+class circle{
+    public: 
+    float area(float rad){
+        return pi*rad*rad; 
+    }
+    float cir(float rad){
+        return pi*rad*2; 
+    }
+};
+int main()
+{
+    float rad,op;
+    int cas;
+    circle circle;
+    p1:
+    cout<<"enter radius of the circle"<<endl;
+    cin>>rad;
+    cout<<"please select an operation\n 1. find area\n 2. find circumference\n";
+    cin>>cas;
+    switch (cas)
+    {
+        case 1:
+            op=circle.cir(rad);
+            cout<<"circumference of circle is "<<op;
+            break;
+        case 2:
+            op=circle.cir(rad);
+            cout<<"circumference of circle is "<<op;
+            break;
+        default:
+        cout<<"invalid choice"<<endl;
+        goto p1;
+    }
+}

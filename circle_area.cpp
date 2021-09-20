c0d4eb02f9fa306a02b37e3e3028bce53edd1115
Ansh Rana <60007155+ranaansh46@users.noreply.github.com>
2021-09-20 21:18:25 +0530

    Update circle_area.cpp

--- circle_area.cpp
+++ circle_area.cpp
@@ -1,38 +1,45 @@
-//program to find the area and circumference of the circle
+//program to find area and circle of the circle
+//written by Ansh Rana
 #include <iostream>
-const float pi=3.1452;
+const float pi = 3.1452; //constant pi
 using namespace std;
-class circle{
-    public: 
-    float area(float rad){
-        return pi*rad*rad; 
+//class with two member functions
+class circle
+{
+public:
+    //function to find area
+    float area(float rad)
+    {
+        return pi * rad * rad;
     }
-    float cir(float rad){
-        return pi*rad*2; 
+    //function to find circumference
+    float cir(float rad)
+    {
+        return pi * rad * 2;
     }
 };
 int main()
 {
-    float rad,op;
+    float rad, op;
     int cas;
     circle circle;
-    p1:
-    cout<<"enter radius of the circle"<<endl;
-    cin>>rad;
-    cout<<"please select an operation\n 1. find area\n 2. find circumference\n";
-    cin>>cas;
+p1:
+    cout << "enter radius of the circle" << endl;
+    cin >> rad;
+    cout << "please select an operation\n 1. find area\n 2. find circumference\n";
+    cin >> cas;
     switch (cas)
     {
-        case 1:
-            op=circle.cir(rad);
-            cout<<"circumference of circle is "<<op;
-            break;
-        case 2:
-            op=circle.cir(rad);
-            cout<<"circumference of circle is "<<op;
-            break;
-        default:
-        cout<<"invalid choice"<<endl;
+    case 1:
+        op = circle.cir(rad);
+        cout << "circumference of circle is " << op;
+        break;
+    case 2:
+        op = circle.cir(rad);
+        cout << "circumference of circle is " << op;
+        break;
+    default:
+        cout << "invalid choice" << endl;
         goto p1;
     }
 }
